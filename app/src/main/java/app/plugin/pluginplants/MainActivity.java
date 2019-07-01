752b4293753037aaac750f8e267d5a977e6ccc0c
hisyammaulana <hisyammaulana100@gmail.com>
2019-07-01 21:46:29 +0700

    first push with Buttom

---
+++ app/src/main/java/app/plugin/pluginplants/MainActivity.java
@@ -0,0 +1,42 @@
+package app.plugin.pluginplants;
+
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.long1.spacetablayout.SpaceTabLayout;
+
+public class MainActivity extends AppCompatActivity{
+    SpaceTabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new StatisticsFragment());
+        fragmentList.add(new ShopFragment());
+        fragmentList.add(new HomeFragment());
+        fragmentList.add(new WikiFragment());
+        fragmentList.add(new SettingsFragment());
+        final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (SpaceTabLayout) findViewById(R.id.spaceTabLayout);
+
+        tabLayout.initialize(viewPager, getSupportFragmentManager(),
+                fragmentList, savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        tabLayout.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+}

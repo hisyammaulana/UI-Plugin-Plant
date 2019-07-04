98dfd148cd351d2f7018944211754024371b7e7b
hisyammaulana <hisyammaulana100@gmail.com>
2019-07-04 21:12:38 +0700

    Ketiga

--- app/src/main/java/app/plugin/pluginplants/WikiFragment.java
+++ app/src/main/java/app/plugin/pluginplants/WikiFragment.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class WikiFragment extends Fragment {
+
+    private CardView cv;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,8 +25,18 @@ public class WikiFragment extends Fragment {
         return rootView;
     }
 
-    public void pindahDetail(View view){
-        Intent intent = new Intent(getActivity(), DetailWikiActivity.class);
-        getActivity().startActivity(intent);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        cv = view.findViewById(R.id.cv1);
+        cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(getContext(), DetailWikiActivity.class);
+                startActivity(a);
+            }
+        });
     }
+
+
 }
